9660e09eef26308e7f873e47f6e66b94f15fc7bf
= <=>
2016-12-13 22:44:27 +0800

    To be continued

---
+++ src/fp_Slicing/NPE.cpp
@@ -0,0 +1,188 @@
+#include <stack>
+#include "NPE.h"
+
+using namespace std;
+
+NPE::NPE() : BinTree<BlockSize>() { }
+
+NPE::NPE(const NPE &orig) : BinTree<BlockSize>(orig) { }
+
+NPE::NPE(const vector<string>& tra, const BlockSizeMap& basicSize)
+{
+    BinNode<BlockSize> *node;
+    stack<BinNode<BlockSize>*> myStack;
+
+    for (size_t i=0; i<tra.size(); i++)
+    {
+        node = new(BinNode<BlockSize>);
+        node->flag = tra[i];
+        if (!isLeaf(node->flag))
+        {
+            node->rchild = myStack.top();
+            myStack.pop();
+            node->lchild = myStack.top();
+            myStack.pop();
+        } else
+        {
+            node->lchild = node->rchild = NULL;
+            node->data = basicSize[node->flag];
+        }
+        myStack.push(node);
+    }
+
+    setRoot(myStack.top());
+    setPostOrder(tra);
+}
+
+NPE::NPE(const string& tra, const BlockSizeMap& basicSize)
+{
+    size_t lptr = 0, rptr;
+    string str = tra + ",";
+    vector<string> traversal;
+
+    while ((rptr=str.find(',',lptr))!=string::npos)
+    {
+        traversal.push_back(str.substr(lptr,rptr-lptr));
+		lptr = rptr + 1;
+    }
+
+    new (this)NPE(traversal, basicSize);
+}
+
+NPE::~NPE() { }
+
+NPE& NPE::operator = (const NPE& i)
+{
+    setRoot(i.root);
+    setPostOrder(i.postOrder);
+    return *this;
+}
+
+BinNode<BlockSize>* NPE::getRoot() const
+{
+    return BinTree<BlockSize>::getRoot();
+}
+
+vector<string> NPE::getPostOrder() const
+{
+    return BinTree<BlockSize>::getPostOrder();
+}
+
+size_t NPE::getArea() const
+{
+    merge(getRoot());
+    return (getRoot()->data.first * getRoot()->data.second);
+}
+
+vector<string> NPE::M1(size_t startPos) const
+{
+    vector<string> result;
+    size_t pos = startPos;
+
+    do {
+        if (pos+2 < postOrder.size())
+            if (isLeaf(postOrder[pos]) && isLeaf(postOrder[pos+1])
+                && isLeaf(postOrder[pos+2]))
+            {
+                result = postOrder;
+                result[pos] = postOrder[pos+1];
+                result[pos+1] = postOrder[pos];
+                break;
+            }
+        pos = (pos+1) % postOrder.size();
+    } while(pos!=startPos);
+
+    return result;
+}
+
+vector<string> NPE::M2(size_t startPos) const
+{
+    vector<string> result;
+    size_t pos = startPos;
+
+    do {
+        if (!isLeaf(postOrder[pos]))
+            if (pos>0 && isLeaf(postOrder[pos-1]))
+                if (pos+1<postOrder.size() && !isLeaf(postOrder[pos+1]))
+                {
+                    result = postOrder;
+                    while (pos < postOrder.size())
+                    {
+                        if (result[pos] == "H")
+                            result[pos] = "V";
+                        else if (result[pos] == "V")
+                            result[pos] = "H";
+                        else break;
+                        pos++;
+                    }
+                    break;
+                }
+        pos = (pos+1) % postOrder.size();
+    } while(pos!=startPos);
+
+    return result;
+}
+
+vector<string> NPE::M3(size_t startPos) const
+{
+    vector<string> result;
+    size_t pos = startPos;
+
+    do {
+        if (pos+1 < postOrder.size())
+            if (isLeaf(postOrder[pos])+isLeaf(postOrder[pos+1]) == 1)
+            {
+                result = postOrder;
+                result[pos] = postOrder[pos+1];
+                result[pos+1] = postOrder[pos];
+                if isLegalNPE(result)
+                    break;
+                else
+                    result.clear();
+            }
+        pos = (pos+1) % postOrder.size();
+    } while(pos!=startPos);
+
+    return result;
+}
+
+bool NPE::isLeaf(const string& str) const;
+{
+    return (str!="H" && str!="V");
+}
+
+bool isLegalNPE(const vector<string>& npe) const
+{
+    size_t numOperand = 0;
+    for (size_t i=0; i<npe.size(); i++)
+    {
+        if (isLeaf(npe[i])) numOperand++;
+
+        // Balloting property
+        if (numOperand <= i+1-numOperand)
+            return false;
+
+        // Normality property
+        if (i>0 && !isLeaf(npe[i-1]) && !isLeaf(npe[i]))
+            if (npe[i-1] == npe[i])
+                return false;
+    }
+    return true;
+}
+
+void NPE::merge(const BinNode<BlockSize>* node)
+{
+    if (node->flag!="H" && node->flag!="V") return;
+
+    merge(node->lchild);
+    merge(node->rchild);
+
+    if (node->flag == "H")
+        node->data = BlockSize(
+            max(node->lchild->data.first, node->rchild->data.first),
+            node->lchild->data.second + node->rchild->data.second);
+    if (node->flag == "V")
+        node->data = BlockSize(
+            node->lchild->data.first + node->rchild->data.first,
+            max(node->lchild->data.second, node->rchild->data.second));
+}
